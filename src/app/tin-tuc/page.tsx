35a8cdfdede619b1838846f0095f2e311ab98c80
khanhha <hanhathoangkhanh@gmail.com>
2024-07-31 17:00:46 +0700

    Add tin-tuc\page.tsx

---
+++ src/app/tin-tuc/page.tsx
@@ -0,0 +1,53 @@
+export default function News() {
+  return (
+    <div className="bg-gray-200">
+      <div className="container mx-auto p-5 ">
+        <div className="my-8 text-neutral-950 text-3xl"  >Bi quyet tim viec</div>
+        <div className="grid grid-cols-4 gap-4">
+          <div>
+            <div>
+            <img src="/img/nhung-cong-viec-lam-tai-nha-anh-dai-dien_626.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Những công việc làm tại nhà kiếm tiền nhanh trong 2024</div>
+          </div>
+          <div>
+            <div>
+            <img src="/img/cac-cong-viec-lam-tai-nha-cho-sinh-vien-anh-dai-dien_860.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Các công việc làm tại nhà cho sinh viên bạn nên thử ngay</div>
+          </div>
+          <div>
+            <div><img src="/img/cac-cong-viec-lam-tai-nha-cho-me-bim-sua-anh-dai-dien_275.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Các công việc làm tại nhà cho mẹ bỉm sữa kiếm thêm thu nhập</div>
+          </div>
+          <div>
+            <div><img src="/img/viec-lam-hanh-chinh-anh-dai-dien_96.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Việc làm hành chính đòi hỏi phẩm chất và kinh nghiệm gì?</div>
+          </div>
+          <div>
+            <div><img src="/img/app-tuyen-dung-viec-lam-anh-dai-dien_40.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">App tuyển dụng việc làm - Công cụ hỗ trợ tìm việc hiệu quả</div>
+          </div>
+          <div>
+            <div><img src="/img/tuyen-dung-viec-lam-24h-anh-dai-dien_165.jpeg" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Tuyển dụng việc làm 24h có còn là trang web hiệu quả?</div>
+          </div>
+          <div>
+            <div><img src="/img/trang-tuyen-dung-viec-lam-uy-tin-anh-dai-dien_101.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Trang tuyển dụng việc làm uy tín giúp bạn có công việc ưng ý</div>
+          </div>
+          <div>
+            <div><img src="/img/tuyen-dung-viec-lam-ke-toan-anh-dai-dien_876.png" alt="" className="w-full h-40" />
+            </div>
+            <div className="p-5 text-lg font-bold leading-normal text-sky-700 overflow-hidden	">Tuyển dụng việc làm kế toán và một số lưu ý bạn cần biết</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
